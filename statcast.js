777fc4b7acf7d227b938f3480e4f0a2fe9105817
PopsPickz <sgaddy10@gmail.com>
2026-07-09 00:26:31 -0400

    Add Statcast data for multiple players

---
+++ statcast.js
@@ -0,0 +1,44 @@
+const StatcastData = {
+  // Example format:
+  // "Juan Soto": {
+  //   barrelRate: 17.2,
+  //   hardHitRate: 55.1,
+  //   exitVelocity: 92.4,
+  //   flyBallRate: 38.7
+  // },
+
+  "Juan Soto": {
+    barrelRate: 17.2,
+    hardHitRate: 55.1,
+    exitVelocity: 92.4,
+    flyBallRate: 38.7
+  },
+
+  "Aaron Judge": {
+    barrelRate: 26.0,
+    hardHitRate: 60.0,
+    exitVelocity: 96.0,
+    flyBallRate: 36.0
+  },
+
+  "Shohei Ohtani": {
+    barrelRate: 19.0,
+    hardHitRate: 55.0,
+    exitVelocity: 94.0,
+    flyBallRate: 39.0
+  },
+
+  "Kyle Schwarber": {
+    barrelRate: 18.0,
+    hardHitRate: 52.0,
+    exitVelocity: 92.5,
+    flyBallRate: 45.0
+  },
+
+  "Pete Alonso": {
+    barrelRate: 16.5,
+    hardHitRate: 50.0,
+    exitVelocity: 91.5,
+    flyBallRate: 44.0
+  }
+};
